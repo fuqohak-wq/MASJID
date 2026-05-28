aae4edb2bf379f9d86e04c1fbf0ea2b6633445d7
fuqohak-wq <fuqohak@uinsuku.ac.id>
2026-05-28 16:14:07 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ======================================================
-// COMBINED SERVICE WORKER (PAV TRACKER + WEBPUSHR)
+// COMBINED SERVICE WORKER (PAV TRACKER + WEBPUSHR + AUDIO)
 // FULL VERSION - TINGGAL COPAS TOTAL
 // ======================================================
 
@@ -18,15 +18,19 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-// 4. Logika Menangkap Sinyal Kebangkitan Tracking Otomatis
+// 4. Logika Menangkap Sinyal Kebangkitan Tracking Otomatis & Suara
 self.addEventListener('push', (event) => {
   console.log("Service Worker: Menerima sinyal Push");
+
+  // Opsi getaran berpola greget (bisa bervariasi tergantung jenis HP)
+  const patternVibrate = [500, 110, 500, 110, 450, 110, 200, 110, 1000, 400, 1000];
 
   // Perintah otomatis membangunkan halaman utama PWA untuk langsung melacak GPS
   event.waitUntil(
     self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
       if (clientList.length > 0) {
-        clientList[0].postMessage({ command: "AUTO_START_TRACKING" });
+        // Jika PWA sedang terbuka/terselip di background, bangunkan fiturnya
+        clientList[0].postMessage({ command: "AUTO_START_TRACKING", play_adzan: true });
       }
     })
   );
